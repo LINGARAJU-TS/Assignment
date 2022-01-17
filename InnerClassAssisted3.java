67d3b6df4aa700e6be2054fdc9690d5aabbb4e9c
LINGARAJU-TS <97216760+LINGARAJU-TS@users.noreply.github.com>
2022-01-17 07:36:59 +0530

    Add files via upload

---
+++ InnerClassAssisted3.java
@@ -0,0 +1,17 @@
+package innerClass;
+	
+
+	public class InnerClassAssisted3 {
+
+	public static void main(String[] args) {
+	AnonymousInnerClass i = new AnonymousInnerClass() {
+
+	         public void display() {
+	            System.out.println("Anonymous Inner Class");
+	         }
+	      };
+	      i.display();
+	   }
+	}
+
+
